17bb6c809a0b282758bc9292c63c0a5910f4badb
randikasptra <saputrarandika857@gmail.com>
2025-05-27 22:34:15 +0700

    add/folder-bp

---
+++ app/Views/pages/bp/bp.php
@@ -0,0 +1,16 @@
+<?= $this->extend('layout/dashboard') ?>
+<?= $this->section('content') ?>
+
+<div class="grid grid-cols-1 md:grid-cols-2 gap-4">
+    <div class="bg-white rounded-xl shadow p-4 border-l-4 border-[#A4DE02]">
+        <h2 class="text-lg font-semibold text-gray-700">Total Pelanggaran Bulan Ini</h2>
+        <p class="text-3xl font-bold text-[#1E5631]">87</p>
+    </div>
+
+    <div class="bg-white rounded-xl shadow p-4 border-l-4 border-red-500">
+        <h2 class="text-lg font-semibold text-gray-700">Siswa Mendekati DO (≥ 180 Poin)</h2>
+        <p class="text-3xl font-bold text-red-600">5</p>
+    </div>
+</div>
+
+<?= $this->endSection() ?>
