5bfdba8545fd5a4beaedcccaa530fc00ac648492
randikasptra <saputrarandika857@gmail.com>
2025-05-27 23:02:22 +0700

    add/pages-admin

--- app/Views/layout/dashboard.php
+++ app/Views/layout/dashboard.php
@@ -5,22 +5,19 @@
     <meta charset="UTF-8">
     <title><?= $title ?? 'Dashboard' ?></title>
     <script src="https://cdn.tailwindcss.com"></script>
-    <script type="module">
-        import {
-            createIcons,
-            icons
-        } from "https://unpkg.com/lucide@latest/dist/lucide.js";
-        window.addEventListener("DOMContentLoaded", () => createIcons({
-            icons
-        }));
-    </script>
-
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap" rel="stylesheet">
     <style>
         body {
             font-family: 'Inter', sans-serif;
         }
     </style>
+    <script src="https://unpkg.com/lucide@latest"></script>
+    <script>
+        document.addEventListener("DOMContentLoaded", function() {
+            lucide.createIcons();
+        });
+    </script>
+
 </head>
 
 <body class="flex min-h-screen bg-[#F0F9F1]">
@@ -38,6 +35,8 @@
             <?= $this->renderSection('content') ?>
         </section>
     </main>
+
+
 </body>
 
 </html>
