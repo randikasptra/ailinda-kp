b0962e4b595782ad28532904325b1e57d91fe940
randikasptra <saputrarandika857@gmail.com>
2025-06-07 10:30:35 +0700

    add/history

---
+++ app/Database/Migrations/2025-06-07-032937_CreateHistoryKonfirmasi.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreateHistoryKonfirmasi extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => ['type' => 'INT', 'auto_increment' => true],
+            'izin_id' => ['type' => 'INT'],
+            'nama' => ['type' => 'VARCHAR', 'constraint' => 100],
+            'kelas' => ['type' => 'VARCHAR', 'constraint' => 10],
+            'waktu_keluar' => ['type' => 'TIME'],
+            'waktu_kembali' => ['type' => 'TIME'],
+            'waktu_kembali_siswa' => ['type' => 'TIME'],
+            'poin_pelanggaran' => ['type' => 'INT'],
+            'created_at' => ['type' => 'DATETIME', 'null' => true],
+        ]);
+
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('history_konfirmasi');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('history_konfirmasi');
+    }
+}
