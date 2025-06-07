2856ba21dfb1ec970f012788195fdda5c74f4c18
randikasptra <saputrarandika857@gmail.com>
2025-06-07 11:02:25 +0700

    add/model-pelanggran

--- app/Database/Seeds/PelanggaranSeeder.php
+++ app/Database/Seeds/PelanggaranSeeder.php
@@ -28,17 +28,7 @@ class PelanggaranSeeder extends Seeder
     }
 }
 
-// 2. Model: PelanggaranModel.php
-namespace App\Models;
 
-use CodeIgniter\Model;
-
-class PelanggaranModel extends Model
-{
-    protected $table = 'pelanggaran';
-    protected $primaryKey = 'id';
-    protected $allowedFields = ['jenis_pelanggaran', 'poin'];
-}
 
 // 3. Controller: Pelanggaran.php
 namespace App\Controllers;
---
+++ app/Models/PelanggaranModel.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class PelanggaranModel extends Model
+{
+    protected $table = 'pelanggaran';
+    protected $primaryKey = 'id';
+    protected $allowedFields = ['jenis_pelanggaran', 'poin'];
+}
